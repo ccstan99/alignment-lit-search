c9ba78a53e73d160ec7ae6cb6d91946637a60dda
ccstan99 <hello@stampy.ai>
2022-12-05 22:24:15 -0800

    streamlit prototype

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,39 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from sentence_transformers import SentenceTransformer
+import pinecone
 
-st.write("""
-# Alignment Literature Search!
-""")
+def run_search(query, model, index):
+    xq = model.encode(query_sentence).tolist()
+    result = index.query(xq, top_k=5, includeMetadata=True)
+    for item in result["matches"]:
+        st.write("### ["+item["metadata"]["title"]+"]("+item["metadata"]["url"]+")")
+        st.write("({0:.2f}) ".format(item["score"]), item["metadata"]["abstract"])
 
-x = st.slider("Select a number")
-st.write("You selected.")
+st.header('Alignment Literature Search')
+
+# x = st.slider("Select a number")
+# option = st.selectbox(
+#      'What is your favorite color?',
+#      ('Blue', 'Red', 'Green'))
+# options = st.multiselect(
+#      'What are your favorite colors',
+#      ['Green', 'Yellow', 'Red', 'Blue'],
+#      ['Yellow', 'Red'])
+# coffee = st.checkbox('Coffee')
+
+query_sentence = st.text_input("What is your question?", value="What is AI Safety?")
+
+# st.secrets["PINECONE_API_KEY"]
+pinecone.init(api_key=st.secrets["PINECONE_API_KEY"], environment='us-west1-gcp')
+model = SentenceTransformer('allenai-specter')
+index = pinecone.Index('alignment-lit')
+
+xq = model.encode(query_sentence).tolist()
+result = index.query(xq, top_k=5, includeMetadata=True)
+for item in result["matches"]:
+    st.write("### ["+item["metadata"]["title"]+"]("+item["metadata"]["url"]+")")
+    st.write("({0:.2f}) ".format(item["score"]), item["metadata"]["abstract"])
+
+# run_search(query_sentence, model, index)
