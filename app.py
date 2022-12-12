fb32ac5642a29778a3be7fc63330fd5073948cc2
ccstan99 <hello@stampy.ai>
2022-12-11 22:06:13 -0800

    highlight with css

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import streamlit as st
-from annotated_text import annotated_text
 import numpy as np
 from sentence_transformers import SentenceTransformer
 from transformers import pipeline
 import pinecone
+import urllib.parse
 
 PINECONE_API_KEY = st.secrets["PINECONE_API_KEY"]
-RETRIEVER_MODEL = 'allenai-specter'
+# RETRIEVER_MODEL = 'allenai-specter'
+RETRIEVER_MODEL = 'multi-qa-mpnet-base-cos-v1'
 READER_MODEL = 'deepset/electra-base-squad2'
 INDEX = 'alignment-lit'
 NAMESPACE = 'faq'
@@ -28,20 +29,27 @@ with st.spinner("Initializing..."):
 
 xq = retreiver.encode(query).tolist()
 result = index.query(xq, namespace=NAMESPACE, top_k=5, includeMetadata=True)
-num_found = 0
+titles = []
 for item in result["matches"]:
     context = item["metadata"]["text"]
     answer = reader(question=query, context=context)
     score = answer["score"]
-    if (score > 0.01):
-        num_found += 1
-        st.subheader("["+item["metadata"]["title"]+"]("+item["metadata"]["url"]+")")
-        annotated_text("({0:.2f}) ".format(answer["score"]), 
-        context[:answer["start"]], (answer["answer"],"","#FDFDC9"), context[answer["end"]:])
+    if (score > 0.001):
+        title = item["metadata"]["title"]
+        url = item["metadata"]["url"]
+        if title in titles:
+            st.write("...")
+        else:
+            titles.append(title)
+            st.subheader("["+title+"]("+url+")")
 
-    # st.markdown("({0:.2f}) ".format(answer["score"]) +
-    # context[:answer["start"]] + "***" + answer["answer"] + "***" + context[answer["end"]:],
-    # unsafe_allow_html=True)
+        answer_text = answer["answer"]
+        url = url + "#:~:text=" +  urllib.parse.quote(answer_text)
+        st.markdown("({0:.3f}) ".format(answer["score"]) +
+        context[:answer["start"]] +
+        "<a href='" + url + "' style='background-color:#FDFDC9'>"+answer_text+"</a>" + 
+        context[answer["end"]:],
+        unsafe_allow_html=True)
 
-if num_found == 0:
+if len(titles) == 0:
     st.write("Sorry, no answer found")
